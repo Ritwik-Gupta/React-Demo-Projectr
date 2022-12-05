9bcf926ad9997435263e7c5764a6518fb9ff897c
Ritwik <ritwik94348gupta@gmail.com>
2022-12-05 21:30:34 +0530

    Cards added

--- src/components/AllExpenses.js
+++ src/components/AllExpenses.js
@@ -1,38 +1,31 @@
 import ExpenseItem from "./ExpenseItem";
-import "./ExpenseItem.css"
+import "./AllExpenses.css"
+import Card from "./Card"
 
 function AllExpenses(props) {
     return(
-        <div>
-            <div>
+        <Card className="expenses">
             <ExpenseItem
                 title = {props.data[0].title}
                 amount = {props.data[0].amount}
                 date = {props.data[0].date}
             />
-            </div>
-            <div>
-                <ExpenseItem
-                    title = {props.data[1].title}
-                    amount = {props.data[1].amount}
-                    date = {props.data[1].date}
-                />
-            </div>
-            <div>
-                <ExpenseItem
+            <ExpenseItem
+                title = {props.data[1].title}
+                amount = {props.data[1].amount}
+                date = {props.data[1].date}
+            />
+            <ExpenseItem
                 title = {props.data[2].title}
                 amount = {props.data[2].amount}
                 date = {props.data[2].date}
-                />
-            </div>
-            <div>
-                <ExpenseItem
+            />
+            <ExpenseItem
                 title = {props.data[3].title}
                 amount = {props.data[3].amount}
                 date = {props.data[3].date}
-                />
-            </div>
-        </div>
+            />
+        </Card>
            
     );
 }
